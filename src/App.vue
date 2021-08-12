3c772fa3897df6b14e03a712a579dea254f9dbe0
alaa <mitra.kh1995@gmail.com>
2021-08-12 17:22:12 +0430

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-app id="inspire">
+      <v-card color="grey lighten-5 my-10 mx-16">
+       <div v-for="i in 10" :key="i">
+         <list-item
+           >
+         </list-item>
+       </div>
+        <div class="text-center">
+          <v-container>
+            <v-row justify="center">
+              <v-col cols="8">
+                <v-container class="max-width">
+                  <v-pagination
+                      v-model="page"
+                      class="my-4"
+                      :length="lastPage"
+                  ></v-pagination>
+                </v-container>
+              </v-col>
+            </v-row>
+          </v-container>
+        </div>
+      </v-card>
+    </v-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import listItem from "./components/listItem";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    listItem
+  },
+  data(){
+    return {
+      page:1,
+      lastPage:15,
+    }
+  },
+  methods:{
+    generateFakeData() {
+
+    }
   }
 }
 </script>
---
+++ src/components/listItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    test2
+  </div>
+</template>
+
+<script>
+export default {
+  name: "listItem"
+}
+</script>
+
+<style scoped>
+
+</style>
